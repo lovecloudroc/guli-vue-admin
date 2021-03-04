e32dda9167873218efb25182b2573fda67952827
zhangpeng <zhangpengaly@163.com>
2021-03-04 22:56:35 +0800

    Initial commit

---
+++ src/views/edu/teacher/list.vue
@@ -0,0 +1,15 @@
+<template>
+  　 <div class="app-container">
+        讲师列表
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'list'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/edu/teacher/save.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-container">
+    讲师添加
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'save'
+}
+</script>
+
+<style scoped>
+
+</style>

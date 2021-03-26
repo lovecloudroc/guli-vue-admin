ea2759adbf2362f42d2a1b0c4e4a74a36e4bf424
zhangpeng <zhangpengaly@163.com>
2021-03-27 00:02:37 +0800

    启用nginx请求转发解决跨域问题

--- src/api/login.js
+++ src/api/login.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function login(username, password) {
   return request({
-    url: '/edu/user/login',
+    url: '/eduservice/user/login',
     method: 'post',
     data: {
       username,
@@ -13,7 +13,7 @@ export function login(username, password) {
 
 export function getInfo(token) {
   return request({
-    url: '/edu/user/info',
+    url: '/eduservice/user/info',
     method: 'get',
     params: { token }
   })
@@ -21,7 +21,7 @@ export function getInfo(token) {
 
 export function logout() {
   return request({
-    url: '/edu/user/logout',
+    url: '/eduservice/user/logout',
     method: 'post'
   })
 }

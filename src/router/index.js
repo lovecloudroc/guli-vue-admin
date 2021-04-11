ea386fa3b40e8fe21a3b210bb40421cb2f98ba43
zhangpeng <zhangpengaly@163.com>
2021-04-11 21:53:39 +0800

    添加课程分类管理路由和界面

--- src/router/index.js
+++ src/router/index.js
@@ -65,6 +65,27 @@ export const constantRouterMap = [
       }
     ]
   },
+  {
+    path: '/subject',
+    component: Layout,
+    redirect: '/subject/table',
+    name: '课程分类管理',
+    meta: { title: '课程分类管理', icon: 'example' },
+    children: [
+      {
+        path: 'table',
+        name: '课程分类列表',
+        component: () => import('@/views/edu/subject/list'),
+        meta: { title: '课程分类列表', icon: 'table' }
+      },
+      {
+        path: 'save',
+        name: '课程分类添加',
+        component: () => import('@/views/edu/subject/save'),
+        meta: { title: '课程分类添加', icon: 'tree' }
+      }
+    ]
+  },
 
   {
     path: '/form',

50c8c63edf9c78b913959ddf6764be76b51e864e
zhangpeng <zhangpengaly@163.com>
2021-07-17 15:51:12 +0800

    prod环境适配

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
+  BASE_API: '"http://192.168.1.113:9001"',
+  OSS_PATH: '"https://cloudroc-edu.oss-cn-beijing.aliyuncs.com"',
 }
